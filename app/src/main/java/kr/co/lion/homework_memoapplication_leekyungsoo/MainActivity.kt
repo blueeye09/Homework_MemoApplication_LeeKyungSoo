785d4b51df7aa83c746872e324bd30e7bc95e193
Kyung Soo Lee <blueeye0909@naver.com>
2024-02-09 15:41:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/lion/homework_memoapplication_leekyungsoo/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.homework_memoapplication_leekyungsoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
